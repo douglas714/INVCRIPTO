64833f73c01587f40e4be77376112139395b8901
Douglas Francisco <douglasnoticias@gmail.com>
2026-06-13 08:01:38 -0300

    Adiciona icone ao executor do conector

--- local-connector/ENVCRIPTO_EXECUTOR.bat
+++ local-connector/ENVCRIPTO_EXECUTOR.bat
@@ -5,6 +5,10 @@ title ENVCRIPTO Executor
 
 cd /d "%~dp0ENVCRIPTO_ARQUIVOS"
 
+if exist "%~dp0INVCRIPTO.ico" if not exist "%~dp0CLIQUE AQUI - ENVCRIPTO.lnk" (
+  powershell -NoProfile -ExecutionPolicy Bypass -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%~dp0CLIQUE AQUI - ENVCRIPTO.lnk'); $s.TargetPath='%~dp0ENVCRIPTO_EXECUTOR.bat'; $s.WorkingDirectory='%~dp0'; $s.IconLocation='%~dp0INVCRIPTO.ico'; $s.Save()" >nul 2>nul
+)
+
 if not exist "INSTALAR_E_EXECUTAR_CONNECTOR.bat" (
   echo.
   echo Nao encontrei a pasta ENVCRIPTO_ARQUIVOS com os arquivos do conector.
